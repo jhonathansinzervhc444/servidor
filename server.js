db67f709c40ff2be82802b9f695ef6c071623c0e
Jhonathan <jhonathansin@hotmail.com>
2025-10-29 15:30:26 -0300

    adicionando rota para mostrar dados

--- server.js
+++ server.js
@@ -14,6 +14,24 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/mensagens', async (req, res) => {
+    try {
+        let dadosAtuais = [];
+
+        try {
+            const dadosArquivo = await fs.promises.readFile(ARQUIVO_DADOS, 'utf-8');
+            dadosAtuais = JSON.parse(dadosArquivo);
+        } catch (readError) {
+            console.warn('dados.json nao encontrado, retornando lista vazia: ', readError);
+        }
+
+        res.status(200).send(dadosAtuais);
+    } catch (error) {
+        console.error('error ao ler dados: ', error);
+        res.send(500).send('error ao ler dados');
+    }
+});
+
 app.post('/salvar-dados', express.json(), async (req, res) => {
     const novoDado = req.body;
 

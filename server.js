26e6da0a53597ad1a39bd780cf689bc7d4e0d445
Jhonathan <jhonathansin@hotmail.com>
2025-10-30 15:59:10 -0300

    alterando logica de cors

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const {body, validationResult} = require('express-validator');
@@ -6,36 +6,75 @@ const cors = require('cors');
 
 const PORT = 3000;
 
-const allowedOrigins = ['http://localhost:3000', 
-    "http://127.0.0.1:3000", 
-    "http://localhost:3000",
-    "http://localhost:3000/salvar-dados",
-    "http://localhost:3000/mensagens"];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3000/'];
 
 const app = express();
 
+/*
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.includes(origin)) {
             console.log("adentrei");
             callback(null, true);
+        } else {
+            console.log("nao adentrei");
+            callback(new Error("CORS nao permitido"));
         }
     }
 }
 
 app.use(cors(corsOptions));
+*/
+app.use((req, res, next) => {
+    const origin = req.headers.origin?.replace(/^http?:\/\//, '');
+
+    if(!origin) {
+        const referer = req.headers.referer;
+        const host = req.headers.host;
+
+        
+        if(referer && referer.includes(host)) {
+            console.log("Conexao sem origin mas referer valido. Permitindo acesso...");
+            console.log(referer);
+            console.log(host);
+            console.log(origin);
+
+            res.setHeader('Acess-Control-Allow-Origin', referer);
+            res.setHeader('Acess-Control-Allow-Credentials', 'true');
+            next();
+        }
+
+        console.log("Conexao sem origin. negando acesso... ", referer);
+        return res.status(403).send("Acesso Negado");
+    }
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Acess-Control-Allow-Origin', origin);
+        res.setHeader('Acess-Control-Allow-Credentials', 'true');
+
+        console.log("entrei aqui?");
+
+        return next();
+    }
+
+    console.warn("Solicitacao bloqueada pelo CORS ", origin);
+
+    return res.status(403).send("Acesso Negado");
+})
+
+
 
 const ARQUIVO_DADOS = path.join(__dirname, 'dados.json');
 
 
 
-app.use(express.static(path.join(__dirname)), cors(corsOptions));
+app.use(express.static(path.join(__dirname)));
 
-app.get('/', cors(corsOptions), (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/mensagens', cors(corsOptions), async (req, res) => {
+app.get('/mensagens', async (req, res) => {
     try {
         let dadosAtuais = [];
 
@@ -53,7 +92,7 @@ app.get('/mensagens', cors(corsOptions), async (req, res) => {
     }
 });
 
-app.post('/salvar-dados', cors(corsOptions), express.json(), body('container-mensagens').escape(), async (req, res) => {
+app.post('/salvar-dados', express.json(), body('container-mensagens').escape(), async (req, res) => {
     const erros = validationResult(req);
     if(!erros.isEmpty()) {
         return res.status(400).json({erros: erros.array()});
@@ -89,34 +128,4 @@ app.post('/salvar-dados', cors(corsOptions), express.json(), body('container-men
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-*/
-
-const cors = require('cors');
-const express = require('express');
-const app = express();
-
-const allowedOrigins = ['http://localhost:8080', 'http://localhost:80'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (e.g., same-origin requests or non-browser clients)
-    if (allowedOrigins.indexOf(origin) !== -1) {
-        console.log("entrei?");
-      callback(null, true);
-    } else {
-        console.log("aaaaaaa", origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
-app.listen(8080, () => {
-  console.log('Server listening on port 8080');
 });
